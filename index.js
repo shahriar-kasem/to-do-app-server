3d90d36cc8cd75aa16f613762f8c60e008782e72
Shahriar Kasem <shahriarkasem@gmail.com>
2022-06-02 20:32:39 +0600

    patch update api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { application } = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,43 +12,61 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ao8vh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
 
         const toDoCollection = client.db('toDoList').collection('notes');
 
-        // post
-        app.post('/add', async(req,res) => {
-            const add = req.body;
-            const result = await toDoCollection.insertOne(add);
-            res.send(result);
-        })
-
         // get
-        app.get('/notes', async(req,res) => {
+        app.get('/notes', async (req, res) => {
             const query = {};
             const result = await toDoCollection.find(query).sort({ _id: -1 }).toArray();
             res.send(result);
         })
 
+        // post
+        app.post('/add', async (req, res) => {
+            const add = req.body;
+            const result = await toDoCollection.insertOne(add);
+            res.send(result);
+        })
+
+        // put
+
+        // patch
+        app.patch('/update', async(req,res) => {
+            const data = req.body;
+            const id = data.id;
+            const complete = data.complete;
+            const filter = {_id: ObjectId(id)}
+            const updateDoc = {
+                $set: {
+                    complete: complete,
+                }
+            }
+            console.log(updateDoc)
+            const result = toDoCollection.updateOne(filter, updateDoc)
+            res.send(result);
+        })
+
         // delete
-        app.delete('/note', async(req,res)=>{
-            
+        app.delete('/note', async (req, res) => {
+
         })
         console.log('Database connected')
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('TO DO APP Server is working fine')
 })
 
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('To Do App Server is listening on port', port)
 })
